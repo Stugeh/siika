1e4f88eebd6406f91744830be5561b95f05f5444
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-03-26 16:50:18 +0200

    added white space and comments. pass indexes by reference

--- gka/parser/lexer.go
+++ gka/parser/lexer.go
@@ -6,98 +6,95 @@ func charAtIndexEq(idx int, source string, cmp string) bool {
 	return string(source[idx]) == cmp
 }
 
-func scanToken(start int, current int, lineNumber int, source string, tokens *[]Token) {
-	lexeme := string(source[current])
+func scanToken(start *int, current *int, lineNumber int, source string, tokens *[]Token) {
+	lexeme := string(source[*start])
 
 	switch lexeme {
 	// *** SINGLE CHAR LEXEMES
 	case "(":
 		*tokens = append(*tokens, CreateToken(LEFT_PAREN, lexeme, "", lineNumber))
-		break
 	case ")":
 		*tokens = append(*tokens, CreateToken(RIGHT_PAREN, lexeme, "", lineNumber))
-		break
 	case "{":
 		*tokens = append(*tokens, CreateToken(LEFT_BRACE, lexeme, "", lineNumber))
-		break
 	case "}":
 		*tokens = append(*tokens, CreateToken(RIGHT_BRACE, lexeme, "", lineNumber))
-		break
 	case ",":
 		*tokens = append(*tokens, CreateToken(COMMA, lexeme, "", lineNumber))
-		break
 	case ".":
 		*tokens = append(*tokens, CreateToken(DOT, lexeme, "", lineNumber))
-		break
 
 	case "-":
 		*tokens = append(*tokens, CreateToken(MINUS, lexeme, "", lineNumber))
-		break
 	case "+":
 		*tokens = append(*tokens, CreateToken(PLUS, lexeme, "", lineNumber))
-		break
 	case ";":
 		*tokens = append(*tokens, CreateToken(SEMICOLON, lexeme, "", lineNumber))
-		break
 	case "*":
 		*tokens = append(*tokens, CreateToken(STAR, lexeme, "", lineNumber))
-		break
 
 	// Lexemes that potentially contain more than one char
 	case "!":
-		if charAtIndexEq(current+1, source, "=") {
+		if charAtIndexEq(*current+1, source, "=") {
 			*tokens = append(*tokens, CreateToken(BANG_EQUAL, "!=", "", lineNumber))
+			*current++
 		} else {
 			*tokens = append(*tokens, CreateToken(BANG, "!", "", lineNumber))
 		}
-		break
 	case "=":
-		if charAtIndexEq(current+1, source, "=") {
+		if charAtIndexEq(*current+1, source, "=") {
 			*tokens = append(*tokens, CreateToken(EQUAL_EQUAL, "==", "", lineNumber))
+			*current++
 		} else {
 			*tokens = append(*tokens, CreateToken(EQUAL, "=", "", lineNumber))
 		}
-		break
 	case "<":
-		if charAtIndexEq(current+1, source, "=") {
+		if charAtIndexEq(*current+1, source, "=") {
 			*tokens = append(*tokens, CreateToken(LESS_EQUAL, "<=", "", lineNumber))
+			*current++
 		} else {
 			*tokens = append(*tokens, CreateToken(LESS, "<", "", lineNumber))
 
 		}
-		break
 
 	case ">":
-		if charAtIndexEq(current+1, source, "=") {
+		if charAtIndexEq(*current+1, source, "=") {
 			*tokens = append(*tokens, CreateToken(GREATER_EQUAL, ">=", "", lineNumber))
+			*current++
 		} else {
 			*tokens = append(*tokens, CreateToken(GREATER, ">", "", lineNumber))
 
 		}
-		break
 
 	// Slash
 	case "/":
-
-		if charAtIndexEq(current+1, source, "=") {
+		if charAtIndexEq(*current+1, source, "/") {
+			// Ignore comments until end of line
+			for *current < len(source) && source[*current] != '\n' {
+				*current++
+			}
+		} else {
+			*tokens = append(*tokens, CreateToken(SLASH, "/", "", lineNumber))
 		}
+
+	// ignore whitespace
+	case " ", "\r", "\t":
+
 	default:
-		logger.Error(lineNumber, "Unexpected character.")
+		logger.Error(lineNumber, "Unexpected character."+lexeme)
 	}
+
 }
 
 func ScanSource(source string) []Token {
-	endReached := false
 	tokens := []Token{}
-	start, current, line := 0, 0, 1
+	start, line := 0, 1
 
-	// While we havent reached the end of source
-	for !(current >= len(source)) {
+	for current := 0; current < len(source); current++ {
+		// Set start of token to current index
 		start = current
-
-		scanToken(start, current, 0, source, &tokens)
-
+		scanToken(&start, &current, line, source, &tokens)
 	}
 
-	return []Token{{Lexeme: "lexeme", Literal: "literal", Line: 10}}
+	return tokens
 }
