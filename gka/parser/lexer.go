928291049e0ad30fc197bf242e71d83a97b19410
Tuukka Veteli <tuukka.veteli@solita.fi>
2024-03-26 14:30:40 +0200

    started work on the lexer

--- gka/parser/lexer.go
+++ gka/parser/lexer.go
@@ -1,11 +1,56 @@
 package parser
 
-type Token struct {
-	Value string
+func scanToken(start int, current int, source string, tokens *[]Token) {
+	lexeme := string(source[start:current])
+
+	switch lexeme {
+	// *** SINGLE CHAR LEXEMES
+	case "(":
+		*tokens = append(*tokens, CreateToken(LEFT_PAREN, lexeme))
+		break
+	case ")":
+		*tokens = append(*tokens, CreateToken(RIGHT_PAREN, lexeme))
+		break
+	case "{":
+		*tokens = append(*tokens, CreateToken(LEFT_BRACE, lexeme))
+		break
+	case "}":
+		*tokens = append(*tokens, CreateToken(RIGHT_BRACE, lexeme))
+		break
+	case ",":
+		*tokens = append(*tokens, CreateToken(COMMA, lexeme))
+		break
+	case ".":
+		*tokens = append(*tokens, CreateToken(DOT, lexeme))
+		break
+
+	case "-":
+		*tokens = append(*tokens, CreateToken(MINUS, lexeme))
+		break
+	case "+":
+		*tokens = append(*tokens, CreateToken(PLUS, lexeme))
+		break
+	case ";":
+		*tokens = append(*tokens, CreateToken(SEMICOLON, lexeme))
+		break
+	case "*":
+		*tokens = append(*tokens, CreateToken(STAR, lexeme))
+		break
+	}
 }
 
-func ScanTokens(source string) []Token {
-	println("Lexing: " + source)
+func ScanSource(source string) []Token {
+	endReached := false
+	tokens := []Token{}
+	start, current, line := 0, 0, 1
 
-	return []Token{{Value: "ast"}, {Value: "best"}}
+	// While we havent reached the end of source
+	for !(current >= len(source)) {
+		start = current
+
+		scanToken(start, current, source, &tokens)
+
+	}
+
+	return []Token{{Lexeme: "lexeme", Literal: "literal", Line: 10}}
 }
